e0c0cf5c607c9f0f5754f27c296fabd39d1e0867
Egor Ermakov <unplaced@gmail.com>
2021-09-12 13:50:26 +0100

    Create alias for mac os apps so they are searchable via Spotlight in MacOs

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 rec {
   # Let Home Manager install and manage itself.
@@ -20,6 +20,43 @@ rec {
   home.stateVersion = "21.05";
 
   nixpkgs.overlays = [ (import ./apps-overlay.nix) ];
+
+  # This enables symlinking of the applications installed via Nix home 
+  # manager to ~/Applications/Nix. See https://github.com/nix-community/home-manager/issues/1341 for
+  # more details. 
+  # Credit for code: https://github.com/ldeck/nix-home 
+  home.activation = {
+    aliasApplications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      app_folder=$(echo ${home.homeDirectory}/Applications/Nix)
+      mkdir -p $app_folder
+
+      IFS=$'\n'
+      old_paths=($(mdfind kMDItemKind="Alias" -onlyin "$app_folder"))
+      new_paths=($(find "$newGenPath/home-path/Applications" -name '*.app' -type l))
+      unset IFS
+
+      old_size="''${#old_paths[@]}"
+      echo "removing $old_size aliased apps from $app_folder"
+      for i in "''${!old_paths[@]}"; do
+        $DRY_RUN_CMD rm -f "''${old_paths[$i]}"
+      done
+
+      new_size="''${#new_paths[@]}"
+      echo "adding $new_size aliased apps into $app_folder"
+
+      for i in "''${!new_paths[@]}"; do
+        real_app=$(realpath "''${new_paths[$i]}")
+        app_name=$(basename "''${new_paths[$i]}")
+        $DRY_RUN_CMD rm -f "$app_folder/$app_name"
+        $DRY_RUN_CMD osascript \
+          -e "tell app \"Finder\"" \
+          -e "make new alias file at POSIX file \"$app_folder\" to POSIX file \"$real_app\"" \
+          -e "set name of result to \"$app_name\"" \
+          -e "end tell"
+      done
+    '';
+  };
+  
 
   # Additional packages
   home.packages = [
